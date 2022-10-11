f474547ef34201ac02f9f00c55bdf75e5628dc96
Danny Elfanbuam <delfanbaum@oreilly.com>
2022-10-11 14:06:54 -0400

    Use support/ files instead of tempfiles

--- spec/merger_spec.rb
+++ spec/merger_spec.rb
@@ -2,34 +2,19 @@ require 'merger'
 require 'tempfile'  # type: ignore
 
 describe AdocFile do
-  let(:test_parent_adoc) do
-    Tempfile.new('adoc').tap do |f|
-      f << "= My Asciidoc File
-
-include::example_child_1.adoc
-
-include::example_child_2.asciidoc
-
-include::example_data.csv"
-      f.close
-    end
-  end
-
-  after do
-    test_parent_adoc.unlink
-  end
+  parent_file = 'spec/support/example_parent.adoc'
 
   describe '#includes?' do
     it 'returns true if there are includes in the file' do
-      parent = AdocFile.new(test_parent_adoc.path)
+      parent = AdocFile.new(parent_file)
       expect(parent.includes?).to eql(true)
     end
   end
 
   describe '#gather_includes' do
     it 'returns an array of included asciidoc files' do
-      parent = AdocFile.new(test_parent_adoc.path)
-      expect(parent.gather_includes).to eql(['example_child_1.adoc',
+      parent = AdocFile.new(parent_file)
+      expect(parent.gather_includes).to eql(['example_child.adoc',
                                              'example_child_2.asciidoc'])
     end
   end

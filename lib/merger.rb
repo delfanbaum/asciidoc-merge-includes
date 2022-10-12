a86fb3bf1b84a7342b2423a558e995d901f50bdf
Danny Elfanbuam <delfanbaum@oreilly.com>
2022-10-12 12:40:40 -0400

    flesh out initial merge_includes functionality

--- lib/merger.rb
+++ lib/merger.rb
@@ -1,23 +1,55 @@
 # Class for files who are going to have their includes merged in
 class AdocFile
-  attr_accessor :text, :includes
+  attr_accessor :text, :includes, :parent_dir
 
   def initialize(filename)
+    @parent_dir = File.dirname(filename)
     @text = File.open(filename).read
-  end
-
-  def includes?
-    !!/^include::/.match(@text)
+    gather_includes
   end
 
   def gather_includes
     return unless includes?
 
-    incl_re = /include::(.*?)\.(adoc|asciidoc)\[(.*?)\]/
-    @includes = @text.scan(incl_re).map do |stub, ext, offset|
-      ["#{stub}.#{ext}", 0] unless offset.match(/leveloffset=/)
+    incl_re = /^include::(.*?)\.(adoc|asciidoc)\[(.*?)\]/
+    incl_list = @text.to_enum(:scan, incl_re).map { Regexp.last_match }
+    @includes = process_incl_list(incl_list)
 
-      ["#{stub}.#{ext}", offset.split('=')[-1].to_i]
+  end
+
+  def merge_includes
+    @includes.map do |line, file, offset|
+      included_file = AdocFile.new("#{parent_dir}/#{file}")
+
+      @text.sub!(line, included_file.text) unless offset != 0
+
+      offset_text = if offset.positive?
+                      [":leveloffset: +#{offset}", ":leveloffset: -#{offset}"]
+                    else
+                      [":leveloffset: #{offset}", ":leveloffset: +#{offset * -1}"]
+                    end
+
+      @text.sub!(line, "#{offset_text[0]}\n\n#{included_file.text}\n\n#{offset_text[1]}")
+    end
+  end
+
+  private
+
+  def includes?
+    incl_re = /^include::(.*?)\.(adoc|asciidoc)\[(.*?)\]/
+    !!incl_re.match(@text)
+  end
+
+  def process_incl_list(match_list)
+    nil unless match_list
+
+    match_list.map do |match|
+      stub = match[1] # file name
+      ext = match[2] # extension
+      attrs = match[3] # leveloffset, if any
+
+      [match[0], "#{stub}.#{ext}", 0] unless attrs.match(/leveloffset=/)
+      [match[0], "#{stub}.#{ext}", attrs.split('=')[-1].to_i]
     end
   end
 end
--- spec/merger_spec.rb
+++ spec/merger_spec.rb
@@ -1,26 +1,55 @@
+require 'spec_helper'
 require 'merger'
 
-describe AdocFile do
-  parent_file = 'spec/support/example_parent.adoc'
-  edgecases_file = 'spec/support/example_edgecases.adoc'
+parent_file = 'spec/support/example_parent.adoc'
+edgecases_file = 'spec/support/example_edgecases.adoc'
 
-  describe '#includes?' do
-    it 'returns true if there are includes in the file' do
-      parent = AdocFile.new(parent_file)
-      expect(parent.includes?).to eql(true)
+describe AdocFile do
+  before(:each) do
+    @parent = AdocFile.new(parent_file)
+  end
+
+  describe '#intialize' do
+    it 'has the expected attributes' do
+      expect(@parent.text).to include('My Asciidoc File')
+      expect(@parent.parent_dir).to include('spec/support')
+      expect(@parent.includes.flatten.to_s).to include('example_child')
     end
   end
 
   describe '#gather_includes' do
     it 'returns an array of included asciidoc files and offsets' do
-      parent = AdocFile.new(parent_file)
-      expect(parent.gather_includes).to eql([['example_child.adoc', 0],
-                                             ['example_child_2.asciidoc', 1]])
+      expect(@parent.gather_includes).to eql([['include::example_child.adoc[]',
+                                               'example_child.adoc', 0],
+                                              ['include::example_child_2.asciidoc[leveloffset=+1]',
+                                               'example_child_2.asciidoc', 1]])
     end
 
     it 'handles negative offsets' do
-      parent = AdocFile.new(edgecases_file)
-      expect(parent.gather_includes).to eql([['some-file.adoc', -44]])
+      edgecase = AdocFile.new(edgecases_file)
+      expect(edgecase.gather_includes).to eql([['include::example_child.adoc[leveloffset=-44]', 'example_child.adoc', -44]])
+    end
+  end
+end
+
+describe 'Merge functionality' do
+  before(:each) do
+    @parent = AdocFile.new(parent_file)
+  end
+
+  describe '.merge_includes' do
+    it 'pulls in included content' do
+      parent = AdocFile.new(parent_file)
+      parent.merge_includes
+      puts parent.text
+      expect(parent.text).to include("I'm A Child File")
+    end
+
+    it 'handles negative offsets' do
+      edgecase = AdocFile.new(edgecases_file)
+      edgecase.merge_includes
+      puts edgecase.text
+      expect(edgecase.text).to include(':leveloffset: -44')
     end
   end
 end

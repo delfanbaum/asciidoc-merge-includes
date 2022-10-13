1f13efbb219a56ee9aaa06ddaec033770ba2d1eb
Danny Elfanbuam <delfanbaum@oreilly.com>
2022-10-13 11:10:44 -0400

    Add basic CLI

---
+++ lib/cli.rb
@@ -0,0 +1,54 @@
+require 'optparse'
+
+# CLI for app via `optparse`
+class Cli
+  # parser for cli
+  class Parser
+    attr_accessor :in_place, :verbose
+
+    def initialize
+      self.in_place = false
+      self.verbose = false
+    end
+
+    def define_options(parser)
+      parser.banner = 'Usage: asciidoc-merge-includes FILE(S) [options]'
+      parser.separator ''
+      parser.separator 'Options:'
+
+      # Add options
+      in_place_option(parser)
+      verbose_option(parser)
+
+      parser.separator ''
+      parser.separator 'Help options:'
+      parser.on_tail('-h', '--help', 'Shows help') do
+        puts parser
+        exit
+      end
+    end
+
+    def in_place_option(parser)
+      parser.on('-ip', '--in-place', 'Modify asciidoc files in place') do
+        self.in_place = true
+      end
+    end
+
+    def verbose_option(parser)
+      parser.on('-v', '--verbose', 'Show verbose processing information') do
+        self.verbose = true
+      end
+    end
+  end
+
+  def parse(args)
+    @options = Parser.new
+    @args = OptionParser.new do |parser|
+      @options.define_options(parser)
+      parser.parse!(args)
+    end
+    @options
+  end
+
+  attr_reader :parser, :options
+end
